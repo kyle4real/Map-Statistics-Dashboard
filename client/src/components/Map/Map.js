988162691dcb32cd5c77c867bc5e400a1b5acc60
Kyle Friel <businessfriel@gmail.com>
2021-07-09 19:10:29 -0400

    Leaflet and react-leaflet installed. Fixed bundling issue.

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,7 +1,33 @@
-import { makeStyles, Container, Typography, Grid } from "@material-ui/core";
+import { makeStyles, Container } from "@material-ui/core";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = () => {
-    return <Container maxWidth="md">map</Container>;
+    const classes = useStyles();
+    return (
+        <div className={classes.map}>
+            <Container maxWidth="md">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </Container>
+            ;
+        </div>
+    );
 };
+
+const useStyles = makeStyles((theme) => ({
+    map: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+}));
 
 export default Map;
