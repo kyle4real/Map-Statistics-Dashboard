d1b54950f4eedc58b96324a76c8213a9dc90bc1c
Kyle Friel <businessfriel@gmail.com>
2021-07-10 20:48:51 -0400

    Structured DataDisplay component.

--- client/src/components/DataDisplay/DataDisplay.js
+++ client/src/components/DataDisplay/DataDisplay.js
@@ -1,8 +1,21 @@
-import { makeStyles, Container, Typography, Grid } from "@material-ui/core";
+import { makeStyles, Container, Typography, Grid, Paper } from "@material-ui/core";
 
 const DataDisplay = () => {
     const classes = useStyles();
-    return <div className={classes.displayData}></div>;
+    return (
+        <div className={classes.displayData}>
+            <Container maxWidth="md">
+                <Grid container spacing={3}>
+                    {/* GRAPH */}
+                    <Grid item xs={12} md={8}>
+                        <Paper></Paper>
+                    </Grid>
+                    {/* DATA FIELDS */}
+                    <Grid item xs={12} md={4}></Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 };
 
 const useStyles = makeStyles((theme) => ({
