78c946e892f5ccec0d44d0841ea9d709329e9b60
Kyle Friel <businessfriel@gmail.com>
2021-07-13 19:05:53 -0400

    Chartjs line chart displayed on DataGraph component.

--- client/src/components/DataDisplay/DataGraph/DataGraph.js
+++ client/src/components/DataDisplay/DataGraph/DataGraph.js
@@ -1,23 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Title from "../Title";
+import { getHistory } from "../../../api/index";
+import { Line } from "react-chartjs-2";
 
 import { makeStyles } from "@material-ui/core";
 
-const DataGraph = () => {
+const DataGraph = ({ country }) => {
     const classes = useStyles();
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await getHistory(country);
+        };
+        getData();
+    }, []);
+
     return (
         <>
             <Title>Data Graph</Title>
-            <div className={classes.dataGraph}></div>
+            <div className={classes.graphContainer}>
+                <Line
+                    width={100}
+                    height={100}
+                    data={{
+                        labels: ["Jan", "Feb", "March", "April"],
+                        datasets: [
+                            {
+                                label: "# of votes",
+                                data: [12, 19, 3, 5, 2, 3],
+                                borderColor: "red",
+                                fill: true,
+                                tension: 0.2,
+                            },
+                        ],
+                    }}
+                    options={{ maintainAspectRatio: false }}
+                />
+            </div>
         </>
     );
 };
 
 const useStyles = makeStyles((theme) => ({
-    dataGraph: {
-        border: "1px solid black",
+    graphContainer: {
         height: "100%",
     },
+    // lineGraph: {
+    //     width: "100%",
+    //     height: "100%",
+    // },
 }));
 
 export default DataGraph;
